7cc005ec723002d0b1f447a5b723dabcddbe0e0c
Thiago Borges <borges@helabs.com.br>
2015-09-17 21:18:32 -0300

    Create Contact model

--- phonebook-api/db/schema.rb
+++ phonebook-api/db/schema.rb
@@ -9,3 +9,18 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150918000932) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "contacts", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "phone",      null: false
+    t.string   "email",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
---
+++ phonebook-api/spec/models/contact_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Contact do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:phone) }
+  end
+  it 'can be created' do
+    contact = create :contact
+    expect(contact).to_not be_nil
+  end
+end
